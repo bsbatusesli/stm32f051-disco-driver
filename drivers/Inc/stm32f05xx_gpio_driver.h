f7677817518cc12f24f8bffc17a32a6a142ca0cd
bsbatusesli <bsbatusesli@gmail.com>
2022-06-03 23:38:38 +0200

    function prototypes created

--- drivers/Inc/stm32f05xx_gpio_driver.h
+++ drivers/Inc/stm32f05xx_gpio_driver.h
@@ -36,5 +36,37 @@ typedef struct
 }GPIO_Handle_t;
 
 
+/*******************
+ *  FUNCTION PROTOTYPES FOR GPIO DRIVER
+ *******************/
+
+
+/* GPIO init and DeInit */
+void GPIO_Init(GPIO_Handle_t *pGPIOx);
+void GPIO_DeInit(GPIO_RegDef_t *pGPIOx);
+
+/* GPIO Peripheral Clock Setup*/
+void GPIO_PeripClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi);
+
+/* Read and write Data */   
+uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber);
+uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *pGPIOx);
+void GPIO_WriteToInputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber, uint8_t Data);
+void GPIO_WriteToInputPort(GPIO_RegDef_t *pGPIOx, uint16_t Data);
+void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber);
+
+/* IRQ Handling */
+void GPIO_IRQConfig(uint8_t IRQNumber, uint8_t IRQPriority, uint8_t EnorDi);
+void GPIO_IRQHandle(uint8_t PinNumber);
+
+
+
+
+
+
+
+
+
+
 
 #endif /* STM32F05XX_GPIO_DRIVER_H_ */
